91dc8906d625a22be12656f5acd3bacc514e7884
rxinwater <ryanaltjekiddo123@gmail.com>
2026-04-15 23:42:49 +0200

    made it yippe

---
+++ PhaseDash/Assets/BarrierDisable.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierDisable : MonoBehaviour
+{
+    [SerializeField] private PlayerMovement playerMovement;
+
+    private Collider2D playerCol;
+    private readonly HashSet<Collider2D> ignored = new HashSet<Collider2D>();
+
+    private void Awake()
+    {
+        playerCol = GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        if (playerMovement.isDashing)
+        {
+            IgnoreAllBarriers();
+        }
+        else
+        {
+            RestoreAll();
+        }
+    }
+
+    private void IgnoreAllBarriers()
+    {
+        Collider2D[] allColliders = FindObjectsOfType<Collider2D>();
+
+        foreach (var col in allColliders)
+        {
+            if (col == null) continue;
+            if (!col.CompareTag("barrier")) continue;
+            if (ignored.Contains(col)) continue;
+
+            Physics2D.IgnoreCollision(playerCol, col, true);
+            ignored.Add(col);
+        }
+    }
+
+    private void RestoreAll()
+    {
+        foreach (var col in ignored)
+        {
+            if (col != null)
+                Physics2D.IgnoreCollision(playerCol, col, false);
+        }
+
+        ignored.Clear();
+    }
+}
