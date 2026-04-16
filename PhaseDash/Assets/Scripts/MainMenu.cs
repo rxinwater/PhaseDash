71db49d5109971e79dea7f3afc24b5842eb7ac99
rxinwater <ryanaltjekiddo123@gmail.com>
2026-04-16 18:36:41 +0200

    Reformat some stuff and added main menu

---
+++ PhaseDash/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Player quit");
+    }
+}
